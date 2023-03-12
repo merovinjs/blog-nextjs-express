986979f7ca9e758ca394089123fdd8d631a22224
Proprogam <k.kartal9595@gmail.com>
2023-03-12 21:35:57 +0300

    dev1 den gelen

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1>kodu dev1 güncelledim</h1>
+      <h1>kodu dev1 den gelen</h1>
     </main>
   );
 }
