91abe91c01bc5e36cded5a0be0d12b547b7e773e
Proprogam <k.kartal9595@gmail.com>
2023-03-12 20:29:05 +0300

    kodu güncelledim

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1>Hello dev Branch pull alma</h1>
+      <h1>kodu güncelledim</h1>
     </main>
   );
 }
