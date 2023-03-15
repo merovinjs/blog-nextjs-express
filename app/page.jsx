11ef369535e968b6b05c414bf46dd12fed16378c
Proprogam <k.kartal9595@gmail.com>
2023-03-15 22:09:50 +0300

    upgrade

---
+++ app/page.jsx
@@ -0,0 +1,22 @@
+import { Inter } from "next/font/google";
+import styles from "./page.module.css";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+export default async function Home() {
+  const data = await fetch(`${process.env.API_URI}/getPosts`, {
+    cache: "no-store",
+  });
+  const res = await data.json();
+  console.log(res);
+  return (
+    <div>
+      <h1>selam</h1>
+      {res.getpost.map((ad) => (
+        <div key={ad._id}>
+          <h2>{ad.name}</h2>
+        </div>
+      ))}
+    </div>
+  );
+}
