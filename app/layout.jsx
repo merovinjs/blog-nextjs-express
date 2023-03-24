00bd04ba760b297ccb263848cda02aa632f2ee71
Proprogam <k.kartal9595@gmail.com>
2023-03-24 15:27:20 +0300

    footer

--- app/layout.jsx
+++ app/layout.jsx
@@ -12,8 +12,8 @@ export default function RootLayout({ children }) {
         <div className={styles.container}>
           <Header />
           <main>{children}</main>
-          <Footer />
         </div>
+        <Footer />
       </body>
     </html>
   );
--- components/Foooter/Footer.jsx
+++ components/Foooter/Footer.jsx
@@ -1,9 +1,38 @@
 import React from "react";
 import styles from "./styles.module.css";
+import logo from "../../public/oldbee2.png";
+import Image from "next/image";
+import adress from "../../public/icon-location.svg";
+import Link from "next/link";
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <h4>Built with Next.js and 💞</h4>
+      <div>
+        <Image
+          className={styles.logo}
+          height={116}
+          src={logo}
+          alt="logo"
+          width={55}
+        />
+      </div>
+      <div className={styles.adress}>
+        <Image src={adress} alt="adress" />
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius facilis
+          dolore doloremque velit.
+        </p>
+      </div>
+      <div className={styles.contact}>
+        <Link href="/">anasayfa</Link>
+        <Link
+          href="https://github.com/oldbeedev
+        "
+        >
+          hakkımda
+        </Link>
+        <Link href="/notlar">bloglar</Link>
+      </div>
     </footer>
   );
 };
