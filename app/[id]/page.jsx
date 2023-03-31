a72b1135f296eb9cdf62eba245b7821f8e3191e2
Proprogam <k.kartal9595@gmail.com>
2023-03-31 22:17:14 +0300

    blogdetail

--- app/[id]/page.jsx
+++ app/[id]/page.jsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from "react";
 import styles from "./styles.module.css";
 const getPost = async (id) => {
@@ -10,7 +11,24 @@ const getPost = async (id) => {
 const Page = async ({ params }) => {
   const detail = await getPost(params.id);
   console.log(detail);
-  return <div className={styles.div}>{detail.detailPost.name}</div>;
+
+  const RandomNumber = () => {
+    return Math.floor(Math.random() * 10);
+  };
+  let Random = RandomNumber();
+
+  return (
+    <div className={styles.blogContainer}>
+      <div className={styles.cardImage}>
+        <Image
+          fill
+          src={`https://picsum.photos/960/400?random=${Random}`}
+          alt="photo"
+        />
+      </div>
+      <div className={styles.div}>{detail.detailPost.name}</div>
+    </div>
+  );
 };
 
 export default Page;
